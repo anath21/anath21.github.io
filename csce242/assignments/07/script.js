e07d2b44f0ce38fde959dfb6787b5a2f1a72debb
Ankit Nath <ankitnath2004@gmail.com>
2024-02-02 17:54:01 -0500

    fixed all 3 functions

--- csce242/assignments/07/script.js
+++ csce242/assignments/07/script.js
@@ -4,12 +4,22 @@ const changeImage = () => {
 };
 
 const addStar = () => {
-    const starBox = document.getElementById("starbox");
-    const star = document.createElement("section");
-    star.add("images/star.png");
+    const starBox = document.getElementById("starBox");
+    const star = document.createElement("img");
+    star.src = "images/star.png";
+    starBox.appendChild(star);
 }
 
+const rotateImage = () => {
+    var slider = document.getElementById("myRange");
+    const image = document.getElementById("image");
+
+    slider.addEventListener("input", () => {
+        const value = slider.value;
+        image.style.transform = `rotate(${value}deg)`;
+    });
+};
+
 document.getElementById("strawberry").onclick = changeImage;
-document.getElementById("button-add-star").onclick = addCircle;
-
-
+document.getElementById("add-star").onclick = addStar;
+rotateImage();
