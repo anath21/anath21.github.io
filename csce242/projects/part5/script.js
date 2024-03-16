e9d264be0659b5a7a700991d647670ae8dbe8c94
Ankit Nath <ankitnath2004@gmail.com>
2024-03-16 17:45:20 -0400

    fixed reading in from json and linked project part 4 and 5 to main page

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -7,36 +7,57 @@ const toggleHamburger = () => {
   };
 
   window.onload = async () => {
-    const propertyListings = document.getElementById('featured-content');
-  
-    try {
-      const response = await fetch('https://github.com/anath21/anath21.github.io/blob/main/csce242/projects/part5/propertues.json');
-      const data = await response.json();
-  
-      data.forEach(property => {
-        const listingDiv = document.createElement('div');
-        listingDiv.classList.add('property-listing');
-  
-        listingDiv.innerHTML = `
-          <img src="images/${property.img_name}" alt="${property.address}">
-          <h2>${property.address}</h2>
-          <p>${property.description}</p>
-          <ul>
-            <li>Bedrooms: ${property.bedrooms}</li>
-            <li>Bathrooms: ${property.bathrooms}</li>
-            <li>Square Footage: ${property.sqft}</li>
-            <li>Price Estimate: ${property.price_estimate}</li>
-            <li>Nearby Schools:</li>
-            ${property.nearby_schools.map(school => `
-              <li>${school.name} - Grades: ${school.grades}, Distance: ${school.distance}</li>
-            `).join('')}
-          </ul>
-        `;
-  
+    const currentPage = window.location.pathname;
+    if (currentPage.includes("scarletOak.html")) {
+      const propertyListings = document.getElementById("scarlet-right");
+      try {
+        const response = await fetch('https://raw.githubusercontent.com/anath21/anath21.github.io/main/csce242/projects/part5/properties.json');
+        const data = await response.json();
+    
+        const scarletData = data.find(property => property._id === 1);
+        const listingDiv = createListingDiv(scarletData);
         propertyListings.appendChild(listingDiv);
-      });
-    } catch (error) {
-      console.log(error);
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (currentPage.includes("spartanDrive.html")) {
+      const propertyListings = document.getElementById("spartan-right");
+      try {
+        const response = await fetch('https://raw.githubusercontent.com/anath21/anath21.github.io/main/csce242/projects/part5/properties.json');
+        const data = await response.json();
+    
+        const spartanData = data.find(property => property._id === 2);
+        const listingDiv = createListingDiv(spartanData);
+        propertyListings.appendChild(listingDiv);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
   
+  function createListingDiv(property) {
+    const listingDiv = document.createElement('div');
+    listingDiv.classList.add('property-listing');
+  
+    listingDiv.innerHTML = `
+      <img src="images/${property.img_name}" alt="${property.address}" class="property-image">
+      <div class="property-details">
+        <h2 class="property-address">${property.address}</h2>
+        <p class="property-description">${property.description}</p>
+        <ul class="property-info">
+          <li>Bedrooms: ${property.bedrooms}</li>
+          <li>Bathrooms: ${property.bathrooms}</li>
+          <li>Square Footage: ${property.sqft}</li>
+          <li>Price Estimate: ${property.price_estimate}</li>
+        </ul>
+        <h3 class="school-header">Nearby Schools:</h3>
+        <ul class="school-list">
+          ${property.nearby_schools.map(school => `
+            <li>${school.name} - Grades: ${school.grades}, Distance: ${school.distance}</li>
+          `).join('')}
+        </ul>
+      </div>
+    `;
+  
+    return listingDiv;
+  };  
